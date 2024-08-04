bda847d5acf8923a2a4d61b82f5569821ba1e05f
earlroyeppen <earl.royeppen@momentum.co.za>
2024-08-04 21:29:38 +0200

    Initial Project Base

---
+++ flash/src/test/java/com/flash/flash/FlashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flash.flash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
