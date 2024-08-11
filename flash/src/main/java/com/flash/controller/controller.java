5338108c6a7bcf8667fce85c5e147cf01315c10e
earlroyeppen <earl.royeppen@momentum.co.za>
2024-08-11 22:07:52 +0200

    Added Swagger library and basic  example

--- flash/src/main/java/com/flash/controller/controller.java
+++ flash/src/main/java/com/flash/controller/controller.java
@@ -1,11 +1,17 @@
 package com.flash.controller;
 
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Tag(name="Sanitise API")
 public class controller {
 
+    @Operation(
+            summary = "Sanitise Keywords in String",
+            description = "Take in a string body and return it with the sql keywords masked")
     @PostMapping("/sanitise")
     public String sanitise(String sentence){
         return sentence;
